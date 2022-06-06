9eb5793c7eb3466314e02dc0ef5b83efda7c0d76
RF_BURGER <arefyazdi4@gmail.com>
2022-06-06 22:21:11 +0430

    creat an esg_api app

---
+++ esg_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EsgApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'esg_api'
--- refinitivApp/settings.py
+++ refinitivApp/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'esg_api',
 ]
 
 MIDDLEWARE = [
