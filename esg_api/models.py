d96acd5c5430105722c29044d84b345154372f4b
RF_BURGER <arefyazdi4@gmail.com>
2022-06-06 23:14:10 +0430

    building a company model

--- esg_api/models.py
+++ esg_api/models.py
@@ -1,3 +1,14 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    title = models.CharField(max_length=255)
+    ticker = models.CharField(max_length=255, unique=True)
+    industry_type = models.TextField(blank=True)
+    rank = models.IntegerField()
+    total_industries = models.IntegerField(validators=[MinValueValidator(0)])
+    esg_score = models.IntegerField()
+    environment_pillar = models.IntegerField()
+    governance_pillar = models.IntegerField()
+    social_pillar = models.IntegerField()
