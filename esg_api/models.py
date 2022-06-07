7cd76e85ff085b624ea7a04decaee34570ddfb9d
RF_BURGER <arefyazdi4@gmail.com>
2022-06-07 10:18:10 +0430

    Extending the company model

--- esg_api/models.py
+++ esg_api/models.py
@@ -2,13 +2,17 @@ from django.core.validators import MinValueValidator
 from django.db import models
 
 
-class Company(models.Model):
+class Corp(models.Model):
     title = models.CharField(max_length=255)
     ticker = models.CharField(max_length=255, unique=True)
     industry_type = models.TextField(blank=True)
+
+
+class ESGScore(models.Model):
     rank = models.IntegerField()
     total_industries = models.IntegerField(validators=[MinValueValidator(0)])
     esg_score = models.IntegerField()
     environment_pillar = models.IntegerField()
     governance_pillar = models.IntegerField()
     social_pillar = models.IntegerField()
+    corp = models.OneToOneField(Corp, on_delete=models.CASCADE, primary_key=True)
