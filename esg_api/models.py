ab722e38486bcd8988040cd16990c06a91b8906a
RF_BURGER <arefyazdi4@gmail.com>
2022-06-15 12:22:07 +0430

    Creating Customer model  Custom permission

--- esg_api/models.py
+++ esg_api/models.py
@@ -71,3 +71,6 @@ class Customer(models.Model):
 
     class Meta:
         ordering = ['user__first_name', 'user__last_name']
+        permissions = [
+            ('view_history', 'Can view history')
+        ]
