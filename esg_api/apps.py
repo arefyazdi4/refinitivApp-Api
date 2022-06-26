831fe552af5c9e7f01e726b14606715a2d37180e
RF_BURGER <arefyazdi4@gmail.com>
2022-06-26 15:59:49 +0430

    Creating signals.py

--- esg_api/apps.py
+++ esg_api/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class EsgApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'esg_api'
+
+    def ready(self):
+        import esg_api.signals
