ac59e7b691e5f5dfb620729318546f1c7d2b88ab
RF_BURGER <arefyazdi4@gmail.com>
2022-06-28 12:38:45 +0430

    Setting up Media file

--- refinitivApp/urls.py
+++ refinitivApp/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 admin.AdminSite.site_header = 'Refinitiv API'
 admin.AdminSite.site_title = 'Admin Panel'
@@ -25,3 +27,6 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
