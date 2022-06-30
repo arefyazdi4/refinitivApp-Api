8525e1992b01655ec893f7ad1211b423bb051f09
RF_BURGER <arefyazdi4@gmail.com>
2022-06-30 19:52:25 +0430

    Creating scraping signal handle

--- esg_api/signals/handlers.py
+++ esg_api/signals/handlers.py
@@ -1,7 +1,8 @@
-from esg_api.models import Customer
+from esg_api.models import Customer, Corp
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.conf import settings
+from . import corp_created
 
 
 # Creating user and Customer in their own app
@@ -9,3 +10,8 @@ from django.conf import settings
 def create_customer_for_new_user(sender, **kwargs):
     if kwargs['created']:  # returns boolean
         Customer.objects.create(user=kwargs['instance'])
+
+
+@receiver(corp_created, sender=Corp)
+def scraping_corps_esg_scores(sender, **kwargs):
+    pass
