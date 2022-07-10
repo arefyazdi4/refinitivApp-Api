96811076279d45c8fd65538ce4ce988e0f2d0520
RF_BURGER <arefyazdi4@gmail.com>
2022-07-10 14:40:07 +0430

    Debugging celery.py

--- refinitivApp/celery.py
+++ refinitivApp/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'refinitivApp')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'refinitivApp.settings')
 
 celery = Celery('refinitivApp')
 celery.config_from_object('django.conf:settings', namespace='CELERY')
