dc68f17ebeb248ad8c4e2bcbeb51292aff95de8f
RF_BURGER <arefyazdi4@gmail.com>
2022-07-11 11:18:15 +0430

    Creating test_corps.py

---
+++ esg_api/tests/test_corps.py
@@ -0,0 +1,10 @@
+from rest_framework.test import APIClient
+from rest_framework import status
+
+
+class TestCreateCorp:
+    def test_if_use_anonymous_return_401(self):
+        client = APIClient()
+        response = client.post(path='/api/corps/', data={'title': 'test_corp_title'})
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
